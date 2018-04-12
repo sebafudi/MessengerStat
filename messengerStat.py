2d62f1c71826fba839062c242a309a07420635d7
Sebastian Fudalej <sebastianfudalej@gmail.com>
2018-04-12 22:54:54 +0200

    Input loop

--- messengerStat.py
+++ messengerStat.py
@@ -4,27 +4,30 @@ start = time.time()
 # Code
 
 import re
-from os import listdir
+import os
 import datetime
 from collections import Counter
 
 allMsgUnFormatted = []
 allMsg = []
-# path = ''
-path = input("Location to Messenger Files: ")
-path = path.replace("\"", "")
-if path == '':
-	path = '.\\MessengerFiles'
 
-for filename in listdir(path):	# for every file in dir
-	if re.match("Counter for Messenger.*?(\d*?).html", filename):	# check for name
-		with open(path + '\\' + filename, encoding='utf-8') as fbFile:	# open file
-			fbFileContent = fbFile.read()	# read file
-		result = re.findall('<div id="(\d*?)" title="(.*?)" time="(.*?)" class="(.*?)"><span class="message-text">(.*?)<\/span>.*?</div>(?s)', fbFileContent, flags=0)	# scan for messages regex
-		for i in result:
-			if i[4] is not '':
-				# allMsg.append((datetime.datetime.strptime(i[2], "%m/%d/%Y %I:%M:%S %p")).month)	# create allMsg tuple
-				allMsg.append((i[0], i[1], datetime.datetime.strptime(i[2], "%m/%d/%Y %I:%M:%S %p"), i[3], i[4]))	# create allMsg tuple
+
+while True:
+	path = input("Location to Messenger Files: ")
+	path = path.replace("\"", "")
+	if path == '':
+		path = '.\\MessengerFiles'
+	if os.path.isdir(path):
+		for filename in os.listdir(path):	# for every file in dir
+			if re.match("Counter for Messenger.*?(\d*?).html", filename):	# check for name
+				with open(path + '\\' + filename, encoding='utf-8') as fbFile:	# open file
+					fbFileContent = fbFile.read()	# read file
+				result = re.findall('<div id="(\d*?)" title="(.*?)" time="(.*?)" class="(.*?)"><span class="message-text">(.*?)<\/span>.*?</div>(?s)', fbFileContent, flags=0)	# scan for messages regex
+				for i in result:
+					if i[4] is not '':
+						# allMsg.append((datetime.datetime.strptime(i[2], "%m/%d/%Y %I:%M:%S %p")).month)	# create allMsg tuple
+						allMsg.append((i[0], i[1], datetime.datetime.strptime(i[2], "%m/%d/%Y %I:%M:%S %p"), i[3], i[4]))	# create allMsg tuple
+		break;
 
 # print(allMsg)
 # allMsg = [(msg[0], msg[1], datetime.datetime.strptime(msg[2], "%m/%d/%Y %H:%M:%S %p"), msg[3], msg[4]) for msg in allMsg]
